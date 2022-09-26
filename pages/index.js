04c765edafe07b87b063a9caffd8f67c9ed1ed43
Vinícius Vieira <viniciusvieiradearaujo@hotmail.com>
2022-09-26 17:45:41 +0000

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,54 @@
+import HomeStyles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the jungle</h1>
+    <div className={HomeStyles.homeContainer}>
+        <div className={HomeStyles.homeHeader}>
+          <div></div>
+          <div className={HomeStyles.homePageTitle} >Avaliable Pages: </div>
+          <div  className={HomeStyles.homeLoginRegisterText}>Login/Register </div>
+        </div>
+        <div className={HomeStyles.homeMiddleContainer}>
+          <div className={HomeStyles.homeInnerContainer}>
+
+            <div className={HomeStyles.userPageContainer}>
+              <div className={HomeStyles.userPageGreenBar}></div>
+              <div className={HomeStyles.userPageTitle}>Vinicius</div>
+              <div className={HomeStyles.userPageSubtitle}>@vinicius</div>
+            </div>
+
+            <div className={HomeStyles.userPageContainer}>
+              <div className={HomeStyles.userPageGreenBar}></div>
+              <div className={HomeStyles.userPageTitle}>Vinicius</div>
+              <div className={HomeStyles.userPageSubtitle}>@vinicius</div>
+            </div>
+
+            <div className={HomeStyles.userPageContainer}>
+              <div className={HomeStyles.userPageGreenBar}></div>
+              <div className={HomeStyles.userPageTitle}>Vinicius</div>
+              <div className={HomeStyles.userPageSubtitle}>@vinicius</div>
+            </div>
+
+            <div className={HomeStyles.userPageContainer}>
+              <div className={HomeStyles.userPageGreenBar}></div>
+              <div className={HomeStyles.userPageTitle}>Vinicius</div>
+              <div className={HomeStyles.userPageSubtitle}>@vinicius</div>
+            </div>
+
+            <div className={HomeStyles.userPageContainer}>
+              <div className={HomeStyles.userPageGreenBar}></div>
+              <div className={HomeStyles.userPageTitle}>Vinicius</div>
+              <div className={HomeStyles.userPageSubtitle}>@vinicius</div>
+            </div>
+
+            <div className={HomeStyles.userPageContainer}>
+              <div className={HomeStyles.userPageGreenBar}></div>
+              <div className={HomeStyles.userPageTitle}>Vinicius</div>
+              <div className={HomeStyles.userPageSubtitle}>@vinicius</div>
+            </div>
+
+          </div>
+        </div>
     </div>
   )
 }
